20f79f3029e581d8bd8a0fce0f13c3f732bff026
Arpit Raj <arpitdubey907@gmail.com>
2021-04-16 12:41:07 +0530

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://morning-sea-83384.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
